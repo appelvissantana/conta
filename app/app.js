7670837a0b4d83338cba2a1dec6dbbc6981ab864
elvissantana <elvissantana.app@gmail.com>
2016-02-15 10:11:36 -0400

    Completando estructura básica del proyecto

--- app/app.js
+++ app/app.js
@@ -1,3 +1,26 @@
 /**
  * Created by Esantana on 2/12/2016.
  */
+
+(function(){
+
+        "use strict";
+        var app = angular.module("conta",["ui.router"]);
+
+        app.config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider)
+         {
+             $urlRouterProvider.otherwise("/");
+
+             $stateProvider
+                 .state("inicio",{
+                     url:"/",
+                     templateUrl:"app/Sigin.html"
+                 })
+
+        }])
+
+
+    }()
+
+
+);
